92425950844fe44d39cd37d64984612845cdfd50
SalaQuangTu <sala.quangtu@gmail.com>
2020-03-26 11:21:48 +0100

    corona bar

---
+++ Assets/Scripts/UI/Corona.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Corona : MonoBehaviour
+{
+    public Slider coronaBar;
+    public int valeurMaxAvantDeces = 100;
+    public int personneDansMaZone = 0;
+
+    private void Start()
+    {
+        personneDansMaZone = 0;
+        coronaBar.maxValue = valeurMaxAvantDeces;
+    }
+
+    private void Update()
+    {
+        coronaBar.value += personneDansMaZone * Time.deltaTime;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        personneDansMaZone++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        personneDansMaZone--;
+    }
+}
