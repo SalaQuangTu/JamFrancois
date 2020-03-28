befa5b49499b32ec9c3f7c440ba5f0bc9bc1cd1a
SalaQuangTu <sala.quangtu@gmail.com>
2020-03-28 15:42:23 +0100

    debut merge hugues

--- Assets/Scripts/hugues/GetAllType.cs
+++ Assets/Scripts/hugues/GetAllType.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GetAllType : MonoBehaviour
+public class grabber : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text score;
+    public int pointPerItem;
+    int totalPoint;
+    public List<GameObject> objectInRange = new List<GameObject>();
+    public LayerMask Items;
+    public Collider[] hitColliders;
+
+    public List<arm> arms = new List<arm>();
+
+    int nbArms;
+   // public arm arm;
+    //Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +25,111 @@ public class GetAllType : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    //    if (nbArms < arms.Count)
+    //    {
+    //        foreach (var item in arms)
+    //        {
+    //            totalPoint += item.GetNumber;
+
+            //            nbArms++;
+            //        }
+            //    }
+
+            score.text = "Score : " + totalPoint * pointPerItem;
         
+        Collider[] hitColliders = Physics.OverlapBox(transform.position, transform.localScale, Quaternion.identity, Items);
+        
+        foreach (var item in hitColliders)
+        {
+            
+            var rand = Random.Range(0f, 1f);
+            objectInRange.Add(item.gameObject);
+            //if (rand < 0.33f)
+            //{
+            
+                //arm.IncreaseSpawnNumber();
+            
+            
+                //for (int i = 0; i < arms.Count; i++)
+                //{
+                //    var z = item.tag;
+                //    if (arms[i].gameObject.tag == z)
+                //    {
+                //        arms[i].IncreaseSpawnNumber();
+                //    }
+                //}
+                //if (arm != null) arm.IncreaseSpawnNumber();
+            //}
+            item.GetComponent<Collider>().enabled = false;
+
+
+            //}
+            //else
+            //{
+            //    Rigidbody rb = item.transform.gameObject.AddComponent<Rigidbody>();
+            //    var dir = transform.parent.position - item.transform.position;
+            //    rb.AddForce(dir, ForceMode.Impulse);
+            //}
+
+        }
+
+        for (int i = 0; i < objectInRange.Count; i++)
+        {
+            objectInRange[i].transform.position = Vector3.Lerp(objectInRange[i].transform.position, transform.parent.position, 5f * Time.deltaTime);
+            for (int y = 0; y < arms.Count; y++)
+            {
+                if(objectInRange[i].tag == arms[y].tag)
+                {
+                    Debug.Log("Add 1  to : " + arms[y].tag);
+                    arms[y].IncreaseSpawnNumber();
+                    totalPoint++;
+                    nbArms = 0;
+                }
+            }
+            Destroy(objectInRange[i], 0.2f);
+            objectInRange.Remove(objectInRange[i]);
+            
+        }
+            //    var dist = Vector3.Distance(transform.parent.position, item.transform.position);
+
+            //    var rand = Random.Range(0f, 1f);
+            //    //if (rand < 0.5f)
+            //    //{
+            //    if (dist < 0.1f)
+            //    {
+
+
+            //        objectInRange.Remove(item);
+            //    }
+            //    //}
+            //    //else
+            //    //{
+            //    //    objectInRange.Remove(item);
+
+            //    //}
+            //}
+            //foreach(var item in collected)
+            //{
+
+            //}
+
+            //foreach (var item in objectInRange)
+            //{
+            //    item.GetComponent<Rigidbody>().AddForce(item.transform.right, ForceMode.Impulse);
+            //}
+        }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Item")
+    //    {
+    //        Debug.Log("sisi : " + other.name);
+    //        /*bjectInRange.Add(other);*/
+    //    }
+    //}
 }
---
+++ Assets/script/hugues/test/arm.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arm : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> Bones;
+    //public GameObject spawn;
+    public float speed;
+    //[Range(1,50)]
+    public int spawnNumber = 0;
+
+    public Transform wantedPos;
+
+    int index;
+    public enum type
+    {
+        snake,
+        pile,
+    }
+
+    public type followType;
+    //public enum 
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //Bones = new List<GameObject>();
+        //var cc =  transform.childCount;
+        //Bones[0] = transform.GetChild(0).gameObject;
+        //Bones[cc] = transform.GetChild(cc).gameObject;
+
+        foreach (Transform child in transform)
+        {
+            Bones.Add(child.gameObject);
+            //}
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bones[0].transform.position = wantedPos.position;
+ 
+        spawnNumber = Mathf.RoundToInt(Mathf.Clamp(spawnNumber, 0, transform.childCount));
+
+            for (int i = 1; i < spawnNumber; i++)
+            {
+                if (Bones[i] != null)
+                {
+                    Bones[i].gameObject.GetComponent<MeshRenderer>().enabled = true;
+                    if (followType == type.pile)
+                    {
+                        Bones[i].transform.position = Vector3.Lerp(Bones[i].transform.position, new Vector3(Bones[i - 1].transform.position.x, Bones[i].transform.position.y, Bones[i - 1].transform.position.z), speed * Time.deltaTime);
+                        Bones[i].transform.rotation = GameObject.Find("Player").transform.rotation;
+                    }
+                    else if (followType == type.snake) if (Vector3.Distance(Bones[i].transform.position, Bones[i - 1].transform.position) > 1f) Bones[i].transform.position = Vector3.Lerp(Bones[i].transform.position, Bones[i - 1].transform.position, speed * Time.deltaTime);
+
+                }
+
+            }
+
+            for (int i = spawnNumber; i < transform.childCount; i++)
+            {
+                Bones[i].gameObject.GetComponent<MeshRenderer>().enabled = false;
+                Bones[i].transform.position = Vector3.Lerp(Bones[i].transform.position, new Vector3(Bones[i - 1].transform.position.x, Bones[i].transform.position.y, Bones[i - 1].transform.position.z), speed * Time.deltaTime);
+            }
+       
+
+        //start.transform.position = Vector3.Lerp(start.transform.parent.position,transform.parent.position, speed * Time.deltaTime);
+        //joint1.transform.position = Vector3.Lerp(joint1.transform.position, new Vector3(start.transform.position.x, joint1.transform.position.y, start.transform.position.z), speed * Time.deltaTime);
+        //joint2.transform.position = Vector3.Lerp(joint2.transform.position, new Vector3(joint1.transform.position.x, joint2.transform.position.y, joint1.transform.position.z), speed * Time.deltaTime);
+        //end.transform.position = Vector3.Lerp(end.transform.position, new Vector3(joint2.transform.position.x, end.transform.position.y, joint2.transform.position.z), speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        for (int i = 0; i < Bones.Count; i++)
+        {
+            if (Bones[i].gameObject != null)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(Bones[i].transform.position, 0.2f);
+                Gizmos.color = Color.yellow;
+                if (i <= Bones.Count - 2) Gizmos.DrawLine(Bones[i].transform.position, Bones[i + 1].transform.position);
+            }
+        }
+
+        //Gizmos.DrawWireSphere(joint2.transform.position, 0.2f);
+        //Gizmos.DrawWireSphere(end.transform.position, 0.4f);
+
+        //Gizmos.DrawLine(start.transform.position, joint1.transform.position);
+        //Gizmos.DrawLine(joint1.transform.position, joint2.transform.position);
+        //Gizmos.DrawLine(joint2.transform.position, end.transform.position);
+    }
+
+    public void IncreaseSpawnNumber()
+    {
+        spawnNumber++;
+    }
+
+}
