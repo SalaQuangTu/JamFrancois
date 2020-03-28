60b587f7678edf596797c89b828493273f83da2c
Wazercube <Wazercube_34@outlook.Fr>
2020-03-28 16:43:45 +0100

    Ajout de tout les sons, d'un script pour jouer la musique avec les events en place mais vide.

---
+++ Assets/Scripts/IAs/IA_Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Spawner : MonoBehaviour
+{
+
+    public float numberOfAIToSpawn = 5;
+    public float minTimerSpawn = 10;
+    public float maxTimerSpawn = 30;
+
+    public GameObject spawnPoint;
+    public GameObject AI_Object;
+
+    private float internTimer = 0;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        internTimer = Random.Range(minTimerSpawn, maxTimerSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > internTimer)
+        {
+            SpawnOne();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    void SpawnOne()
+    {
+        Instantiate(AI_Object, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+}
