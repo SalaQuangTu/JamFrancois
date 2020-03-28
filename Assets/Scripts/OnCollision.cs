b10c43cffa4acbbc0b3ebe4ffad6f1c45cb2c46f
Wazercube <Wazercube_34@outlook.Fr>
2020-03-28 14:56:51 +0100

    Ajout des events et des sons de cadis. Manque l'intgration avec tout ce qui touche au joueur ( vitesse etc)

---
+++ Assets/Scripts/OnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    public CollisionEvents collision;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        CollisionInfos info = new CollisionInfos
+        {
+            objectCollided = col.gameObject,
+            maskOfObjectCollided = col.gameObject.layer       
+        };
+
+        collision.Invoke(info);
+    }
+}
---
+++ Assets/Scripts/Sounds/SoundsOnCollision.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundsOnCollision : MonoBehaviour
+{
+    public AudioClip[] collideSounds = new AudioClip[0];
+    public string tagToTest = "";
+    public LayerMask maskToTest = 0;
+    [Range(0,1)]
+    public float volume = 1;
+
+
+    private int lastOnePlayed = -1;
+    
+
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlaySoundOnCollisionTagBased(CollisionInfos info)
+    {
+        if (source)
+        {
+            if (collideSounds.Length > 1)
+            {
+                if (info.objectCollided.CompareTag(tagToTest))
+                {
+                    int rand = 0;
+                    int breakLoop = 0;
+                    do
+                    {
+                        breakLoop++;
+                        rand = Random.Range(0, collideSounds.Length);
+                    } while (rand == lastOnePlayed && breakLoop < 10);
+                    source.PlayOneShot(collideSounds[rand], volume);
+                    lastOnePlayed = rand;
+                }
+            }
+            else
+            {
+                source.PlayOneShot(collideSounds[0], volume);
+            }
+        }
+        else
+        {
+            Debug.Log("SoundsOnCollision: AudioSource = null", gameObject);
+        }
+    }
+
+
+    public void PlaySoundOnCollisionMaskBased(CollisionInfos info)
+    {
+        if (source)
+        {
+            if (collideSounds.Length > 1)
+            {
+                if (((1<<info.maskOfObjectCollided) & maskToTest)!=0)
+                {
+                    int rand = 0;
+                    int breakLoop = 0;
+                    do
+                    {
+                        breakLoop++;
+                        rand = Random.Range(0, collideSounds.Length);
+                    } while (rand == lastOnePlayed && breakLoop < 10);
+                    source.PlayOneShot(collideSounds[rand], volume);
+                    lastOnePlayed = rand;
+                }
+            }
+            else
+            {
+                source.PlayOneShot(collideSounds[0], volume);
+            }
+        }
+        else
+        {
+            Debug.Log("SoundsOnCollision: AudioSource = null", gameObject);
+        }
+    }
+
+}
