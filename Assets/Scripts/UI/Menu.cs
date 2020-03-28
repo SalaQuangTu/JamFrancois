7115204374e1ffe0bc221da7e8788e90ac1b2228
SalaQuangTu <sala.quangtu@gmail.com>
2020-03-28 15:21:08 +0100

    menu fait police faites transition fait build set up

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Transition.Instance.LoadNextLevel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
